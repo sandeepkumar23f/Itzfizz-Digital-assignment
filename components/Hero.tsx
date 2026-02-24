0d140ab1e8a8cc513b83e10b52929e8a49b9ada6
sandeepkumar23f <sandeep21000552@gmail.com>
2026-02-24 17:26:52 +0530

    the basic working of this project is done now i will enhance it

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,68 @@
 "use client";
 
-import { useRef, useEffect } from "react";
+import { useEffect, useRef } from "react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Hero() {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top top",
+          end: "+=2500",
+          scrub: 1,
+          pin: true,
+          anticipatePin: 1,
+        }
+      });
+
+      tl.to(".car", {
+        x: "70vw",
+        ease: "none",
+      }, 0);
+
+      tl.to(".text-mask", {
+        clipPath: "inset(0% 0% 0% 0%)",
+        ease: "none",
+      }, 0);
+
+    }, containerRef);
+
+    return () => ctx.revert();
+  }, []);
 
   return (
-    <>
-      <section className="h-screen flex items-center justify-center bg-black text-white">
-        <h1 className="text-xl md:text-7xl tracking-[0.6rem] font-light text-center">WELCOME&nbsp;TO&nbsp;ITZFIZZ</h1>
-      </section>
-    </>
+    <section
+      ref={containerRef}
+      className="hero relative h-screen bg-black flex items-center"
+    >
+
+      <div className="absolute left-20 z-10">
+        <div
+          className="text-mask"
+          style={{
+            clipPath: "inset(0% 100% 0% 0%)", // initially the text will be hidden
+          }}
+        >
+          <h1 className="text-white text-6xl font-bold whitespace-nowrap">
+            Welcome to Itzfizz Digital
+          </h1>
+        </div>
+      </div>
+
+      <img
+        src="/car.png"
+        alt="Car"
+        className="car absolute left-0 w-96"
+      />
+
+    </section>
   );
 }
